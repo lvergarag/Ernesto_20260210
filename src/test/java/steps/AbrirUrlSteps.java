8401cc4ff138a80bf3f73e5c10157a992ad942f5
LVG <lvergarag@gmail.com>
2026-02-13 10:43:25 -0300

    5to   commit ethhasthrdjmdjh  Ernesto 2

--- src/test/java/steps/AbrirUrlSteps.java
+++ src/test/java/steps/AbrirUrlSteps.java
@@ -26,9 +26,7 @@ public class AbrirUrlSteps {
     public void click_en_Realizar_una_consulta() throws InterruptedException{
         basePage.hacerClickEnConsulta();
         System.out.println("01 click en Realizar Conulta");
-
         String mensajeEsperado = "Ready to Make Your Move?";
-
 
         Assert.assertEquals("El mensaje de consola no es el esperado",
                 "Ready to Make Your Move?", mensajeEsperado);
@@ -36,19 +34,16 @@ public class AbrirUrlSteps {
 
     @And("mostrar por consola el aviso")
     public void mostrar_por_consola_el_aviso() throws InterruptedException{
-       System.out.println("02 Se muestra el aviso:  Ready to Make Your Move?");
+       System.out.println("02 Se muestra el aviso:  Ready to Make Your Move? ");
     }
 
     @Then("validar aviso")
     public void validar_aviso() {
         String mensajeEsperado = "Ready to Make Your Move?";
-
         System.out.println(" ");
-        System.out.println("03 Text_AddProduct");
-        System.out.flush();
+        System.out.println("03 Text_AddProduct ");
         String Text_AddProduct = driver.findElement(By.cssSelector("div.p-12.text-center h2.text-4xl.md\\:text-5xl.font-bold.mb-6")).getText();
         System.out.println(Text_AddProduct);
-        System.out.flush();
         System.out.println(" ");
 
       //  Assert.assertNotEquals("El mensaje de consola NO es el esperado",
@@ -56,12 +51,12 @@ public class AbrirUrlSteps {
                 "Ready to Make Your Move?",
                 mensajeEsperado);
 
-        System.out.println("04 Validación exitosa: El aviso es correcto.");
+        System.out.println("04 Validación exitosa: El aviso es correcto. ");
 
     }
 
     @And("la pagina debe cargar")
     public void la_pagina_debe_cargar()  {
-        System.out.println("01.1 Título de la página: " + driver.getTitle());
+        System.out.println("01.1 Título de la página : " + driver.getTitle());
     }
 }
